9f635d9dbac6dbf0142a11056850d2db3938fcfa
Leroy Souz <leroy.souz@gmail.com>
2024-01-31 08:08:29 -0500

    saving model command

--- train.py
+++ train.py
@@ -4,9 +4,11 @@ from model import *
 NUM_EPOCHS = 10
 
 for epoch in range(1,NUM_EPOCHS+1):
-  start_time = timer()
-  train_loss = train_epoch(transformer, optimizer)
-  end_time = timer()
-  val_loss = evaluate(transformer)
-  print(f'Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}')
-  print(f'Epoch time: {(end_time - start_time):.3f}s')
+    start_time = timer()
+    train_loss = train_epoch(model, optimizer)
+    end_time = timer()
+    val_loss = evaluate(model)
+    print(f'Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}')
+    print(f'Epoch time: {(end_time - start_time):.3f}s')
+    
+torch.save(model.state_dict(), 'model.pth')
