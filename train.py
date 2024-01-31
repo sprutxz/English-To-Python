59bc550ba4ced3bd7101f7fab286018c5c9169fc
Leroy Souz <leroy.souz@gmail.com>
2024-01-31 12:25:46 -0500

    updates # of epochs

--- train.py
+++ train.py
@@ -1,14 +1,14 @@
 from model import *
 
 # Training the model
-NUM_EPOCHS = 10
-
+NUM_EPOCHS = 50
+model.load_state_dict(torch.load('model.pth'))
 for epoch in range(1,NUM_EPOCHS+1):
     start_time = timer()
     train_loss = train_epoch(model, optimizer)
     end_time = timer()
     val_loss = evaluate(model)
-    print(f'Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}')
-    print(f'Epoch time: {(end_time - start_time):.3f}s')
+    print(f'Epoch: {epoch}, Train loss: {train_loss:.3f}, Val loss: {val_loss:.3f}', flush=True)
+    print(f'Epoch time: {(end_time - start_time):.3f}s', flush=True)
     
 torch.save(model.state_dict(), 'model.pth')
